d088b71d8b4e3e87d59cfa82b70c24e73ae40a9d
Austin Felipe <austinfelipe@Austins-Mac.local>
2016-01-19 20:01:30 -0200

    Initial commit

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Int
+var num = -500
+var numTyped: Int = -500
+
+// Unsigned Int
+var uNum: UInt = 200
---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// String
+var str = "Hello, playground"
+
+// Integer
+var num = 20
+
+// Decimal
+var decimal = 50.5
+
+// Changing a variable's value
+decimal = 20.23
+
+// Constants
+let newDecimal = 40.5
+
+// Declaring multiple variables in one line
+var test1 = 1, test2 = 1.1, test3 = "teste"
+
+// Declaring variables with type
+var newMessage: String
